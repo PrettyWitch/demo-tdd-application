87ee1a3246140cae3b1a1a8050dd2bfda3449332
yuhan <dongyuhanyao@126.com>
2022-12-04 10:48:00 +0800

    feat: generate project skeleton

---
+++ src/test/java/com/tw/capability/gtb/demotddapplication/DemoTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.capability.gtb.demotddapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
