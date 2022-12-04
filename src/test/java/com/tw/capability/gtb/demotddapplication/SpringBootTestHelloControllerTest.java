9854a9b65f227925f43f9cb9b5a44ce8835300d7
yuhan <dongyuhanyao@126.com>
2022-12-04 11:00:39 +0800

    feat:add hello world endpoint with system testing

---
+++ src/test/java/com/tw/capability/gtb/demotddapplication/SpringBootTestHelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.tw.capability.gtb.demotddapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringBootTestHelloControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void should_return_hello_world() {
+        // given
+
+        // when
+        ResponseEntity<String> response = restTemplate.getForEntity("/hello", String.class);
+
+        // then
+        assertThat(response.getBody()).isEqualTo("Hello World!");
+    }
+}
